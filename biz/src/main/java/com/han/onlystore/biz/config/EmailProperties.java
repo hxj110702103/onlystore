a6076dd3c7e19e9f0025d1cb67e61b741977ff27
hxj110702103 <923575965@qq.com>
2017-03-22 16:16:39 +0800

    add javamail

---
+++ biz/src/main/java/com/han/onlystore/biz/config/EmailProperties.java
@@ -0,0 +1,61 @@
+package com.han.onlystore.biz.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Amy on 2017/3/22.
+ */
+@ConfigurationProperties(prefix = "email")
+@Component
+public class EmailProperties {
+    private String  mailSmtpHost;
+
+    private String mailTransportProtocol;
+
+    private String mailSmtpAuth;
+
+    private String username;
+
+    private String password;
+
+    public String getMailSmtpHost() {
+        return mailSmtpHost;
+    }
+
+    public void setMailSmtpHost(String mailSmtpHost) {
+        this.mailSmtpHost = mailSmtpHost;
+    }
+
+    public String getMailTransportProtocol() {
+        return mailTransportProtocol;
+    }
+
+    public void setMailTransportProtocol(String mailTransportProtocol) {
+        this.mailTransportProtocol = mailTransportProtocol;
+    }
+
+    public String getMailSmtpAuth() {
+        return mailSjamtpAuth;
+    }
+
+    public void setMailSmtpAuth(String mailSmtpAuth) {
+        this.mailSmtpAuth = mailSmtpAuth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- biz/src/main/java/com/han/onlystore/biz/utils/JavaMail.java
+++ biz/src/main/java/com/han/onlystore/biz/utils/JavaMail.java
@@ -1,5 +1,7 @@
 package com.han.onlystore.biz.utils;
 
+import com.han.onlystore.biz.config.EmailProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import sun.plugin2.message.Message;
 
@@ -16,32 +18,32 @@ import java.util.Properties;
 @Component
 public class JavaMail {
 
-    private static final String HOST = "smtp.163.com";
+    @Autowired
+    private EmailProperties emailProperties;
 
     public void sendMail(String to, String code) throws MessagingException {
         //1.创建连接对象，连接邮箱服务器
         Properties props= new Properties();
-        props.setProperty("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.host", HOST);
-        props.setProperty("mail.smtp.auth", "true");
+        props.setProperty("mail.transport.protocol", emailProperties.getMailTransportProtocol());
+        props.put("mail.smtp.host", emailProperties.getMailSmtpHost());
+        props.setProperty("mail.smtp.auth", emailProperties.getMailSmtpAuth());
 
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("m18233138990@163.com", "hxj123456");
+                return new PasswordAuthentication(emailProperties.getUsername(), emailProperties.getPassword());
             }
         });
 
         //2.创建邮件对象
         MimeMessage message =   new MimeMessage(session);
-        Address fromAddress = new InternetAddress("m18233138990@163.com");
+        Address fromAddress = new InternetAddress(emailProperties.getUsername());
         message.setFrom(fromAddress);
-        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("m13817556575@163.com"));
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("m13817556575@163.com")); //这里将接收邮件人，设为了固定值
         message.setSubject("账号激活邮件");
         message.setContent("账号激活邮件，点击以下连接激活", "text/html;charset=utf-8");
 
         //3.发送邮件
         Transport.send(message);
     }
-
 }
