d9b9ccdfe58a1a42e44d09e90e9e2901401d10b4
hxj110702103 <923575965@qq.com>
2017-03-28 18:37:29 +0800

    微信网页认证

---
+++ web/src/main/java/com/han/onlystore/web/controller/MemberController.java
@@ -0,0 +1,48 @@
+package com.han.onlystore.web.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.han.onlystore.web.utils.AuthClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+/**
+ * Created by Amy on 2017/3/28.
+ */
+@RestController
+@RequestMapping("member")
+public class MemberController {
+
+    @RequestMapping("index")
+    public void index(HttpServletResponse response) throws IOException {
+
+        String backUrl = "http://anycall.ants-city.com/member/urlCallback";
+        String url = "https://open.weixin.qq.com/connect/oauth2/authorize?" +
+                "appid=" + AuthClient.AppID +
+                "&redirect_uri=" + URLEncoder.encode(backUrl) +
+                "&response_type=code" +
+                "&scope=snsapi_userinfo" +
+                "&state=STATE#wechat_redirect";
+
+        response.sendRedirect(url);
+    }
+
+    @RequestMapping("urlCallback")
+    public void urlCallback(HttpServletRequest request) throws IOException {
+        String code = request.getParameter("code");
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?" +
+                "appid=" + AuthClient.AppID +
+                "&secret=" + AuthClient.AppSecret +
+                "&code=" + code +
+                "&grant_type=authorization_code ";
+        JSONObject jsonObject = AuthClient.doGetJson(url);
+        String openid = jsonObject.getString("openid");
+        String accessToken = jsonObject.getString("access_token");
+    }
+}
---
+++ web/src/main/java/com/han/onlystore/web/utils/AuthClient.java
@@ -0,0 +1,36 @@
+package com.han.onlystore.web.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+/**
+ * Created by Amy on 2017/3/28.
+ */
+public class AuthClient {
+
+    public static final String AppID="wx2069adc5917f2373";
+    public static final String AppSecret="751aeb9a0be0eb985a87e3235a0019eb";
+
+    public static JSONObject doGetJson(String url) throws IOException {
+        JSONObject jsonObject = new JSONObject();
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URLEncoder.encode(url));
+        HttpResponse response = client.execute(httpGet);
+
+        HttpEntity entity = response.getEntity();
+        if(entity != null) {
+            String str = EntityUtils.toString(entity);
+            jsonObject = JSONObject.parseObject(str);
+        }
+        httpGet.releaseConnection();
+
+        return jsonObject;
+    }
+}
